21e9a94088682f88b60e7f2dca70d733d622fa54
Sandesh <sandeshchalawadi16@gmail.com>
2026-05-17 23:54:00 +0530

    Fixed Vercel build error

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Login.css";
-import { useNavigate } from "react-router-dom";
+import { } from "react-router-dom";
 
 function Login() {
 
